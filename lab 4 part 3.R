988274784bb1bd16a38a60aa9a31d23114fc9791
kaitlynlandfield <kaitlynlandfield@gmail.com>
2019-10-21 13:56:48 -0700

    added a line graph

--- lab 4 part 3.R
+++ lab 4 part 3.R
@@ -10,4 +10,17 @@ library(here)
 
 db <- read_csv(here::here ("data", "disease_burden 2.csv")) %>% 
   clean_names() %>% 
-  rename(deaths_per_100k = death_rate_per_100_000)
+  rename(deaths_per_100k = death_rate_per_100_000) 
+
+#Create a subset and graph
+
+db_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+#Create a basic line graph
+
+ggplot(data = db_sub, aes(x = year, y = deaths_per_100k))+
+  geom_line(aes(color = country_name))
+
+
